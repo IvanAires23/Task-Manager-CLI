bfe195c80bcbbc6cce89edb030c4139df9cb363e
IvanAires23 <ivanaires32@gmail.com>
2025-06-10 16:06:20 -0300

    feat: implementa listagem de tarefas formatada com tabulate

--- app/main.py
+++ app/main.py
@@ -1,12 +1,5 @@
 from db import create_tables
-from tasks import add_task
+from tasks import list_tasks
 
 create_tables()
-
-add_task(
-    title="Estudar Python",
-    description="Terminar projeto do gerenciador de tarefas",
-    due_date="2025-06-10",
-    priority="alta",
-    tags="estudo,programação"
-)
+list_tasks()
--- app/tasks.py
+++ app/tasks.py
@@ -1,4 +1,5 @@
 from db import connect
+from tabulate import tabulate
 
 
 def add_task(title, description, due_date, priority, tags):
@@ -13,3 +14,23 @@ def add_task(title, description, due_date, priority, tags):
     conn.commit()
     conn.close()
     print("Tarefa adicionada com sucesso.")
+
+
+def list_tasks():
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT id, title, due_date, priority, tags, status FROM tasks"
+        )
+    tasks = cursor.fetchall()
+
+    conn.close()
+
+    if tasks:
+        headers = [
+            "ID", "Título", "Vencimento", "Prioridade", "Etiquetas", "Status"
+            ]
+        print(tabulate(tasks, headers=headers, tablefmt="fancy_grid"))
+    else:
+        print("Nenhuma tarefa encontrada.")
